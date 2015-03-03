58a595397acec196842c4bba60987425aff175bf
Dominique Septembre <dominique.septembre@gmail.com>
2015-03-03 14:07:16 -0500

    augumenter_la_capacite_d_un_dynamicArray_devrait_preserver_le_contenu_deja_present
    augumenter_la_capacite_d_un_dynamicArray_devrait_preserver_le_contenu_deja_present

--- dynamicArray/DynamicArray.hpp
+++ dynamicArray/DynamicArray.hpp
@@ -6,6 +6,58 @@ DynamicArray<T>::DynamicArray()
 }
 
 template <class T>
+DynamicArray<T>::DynamicArray(int _capacite)
+{
+	capacite = _capacite;
+	tabElements = new T[_capacite];
+
+	for (int i = 0; i < capacite; i++)
+	{
+		tabElements[i] = T();
+	}
+}
+
+template <class T>
+void DynamicArray<T>::operator=(const DynamicArray<T>& _oldArray)
+{
+	if (this != &_oldArray){
+		this->setCapacite(_oldArray.getCapacite());
+
+		for (int i = 0; i < capacite; i++)
+		{
+			tabElements[i] = _oldArray.getElement(i);
+		}
+	}
+}
+
+template <class T>
+bool DynamicArray<T>::operator==(const DynamicArray<T>& _compareArray) const
+{
+	bool equals = false;
+
+	if (this->capacite == _compareArray.getCapacite())
+	{
+		for (int i = 0; i < capacite; i++)
+		{
+			if (tabElements[i] == _compareArray.getElement(i))
+			{
+				equals = true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+	else
+	{
+		equals = false;
+	}
+
+	return equals;
+}
+
+template <class T>
 int DynamicArray<T>::getCapacite() const
 {
 	return capacite;
@@ -21,5 +73,25 @@ template <class T>
 void DynamicArray<T>::setElement(const int _index, const T& _valeur)
 {
 	tabElements[_index] = _valeur;
+}
+
+template <class T>
+void DynamicArray<T>::setCapacite(const int _capacite)
+{
+	if (_capacite < 1) throw std::invalid_argument("Le tableau ne peut pas être vide");
+
+	T * tempTabElement = new T[_capacite];
+	for (int i = 0; i < _capacite; i++)
+	{
+		if (i < capacite)
+			tempTabElement[i] = tabElements[i];
+		else
+			tempTabElement[i] = T();
+	}
+
+	capacite = _capacite;
+
+	delete tabElements;
+	tabElements = tempTabElement;
 }
 
--- dynamicArray/dynamicArray.h
+++ dynamicArray/dynamicArray.h
@@ -6,10 +6,15 @@ class DynamicArray
 {
 public:
 	DynamicArray();
+	DynamicArray(int _capacite);
+	bool operator==(const DynamicArray<T> & _compareArray) const;
 	int getCapacite() const;
 	T getElement(const int _pos) const;
 	void setElement(const int _index,const T & _valeur);
+	void setCapacite(const int _capacite);
 private:
+	//DynamicArray(const DynamicArray<T> & _oldArray);
+	void operator=(const DynamicArray<T> & _oldArray);
 	T * tabElements;
 	int capacite;
 };
